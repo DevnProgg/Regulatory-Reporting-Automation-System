86e4bbcb0a00e1dce03c681193e21b0e15d50d8c
Lauren Qokolo <128149241+DevnProgg@users.noreply.github.com>
2026-02-03 17:00:08 +0200

    init

---
+++ Backend/rras/src/main/java/com/wisetech/rras/RrasApplication.java
@@ -0,0 +1,13 @@
+package com.wisetech.rras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrasApplication.class, args);
+	}
+
+}
---
+++ Backend/rras/src/test/java/com/wisetech/rras/RrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisetech.rras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
