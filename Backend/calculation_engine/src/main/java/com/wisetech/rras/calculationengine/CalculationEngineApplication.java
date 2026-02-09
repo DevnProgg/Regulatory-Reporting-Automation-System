59fefbb93995eca4e5ead23f6b00d473c3f4205e
Lauren Qokolo <128149241+DevnProgg@users.noreply.github.com>
2026-02-09 12:32:23 +0200

    addition of calculation engine
    addition of calculation engine

---
+++ Backend/calculation_engine/src/main/java/com/wisetech/rras/calculationengine/CalculationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.wisetech.rras.calculationengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationEngineApplication.class, args);
+	}
+
+}
---
+++ Backend/calculation_engine/src/test/java/com/wisetech/rras/calculationengine/CalculationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisetech.rras.calculationengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
